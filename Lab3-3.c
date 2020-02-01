fa18ea0b7caea3bf5e2883d953eed42e1267bc64
RYU <ryuchotichan@gmail.com>
2020-02-01 12:08:51 -0800

    lab3-3

--- Lab3-3.c
+++ Lab3-3.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 
 int main(){
-    float num1;
-    scanf("%f", &num1);
+    int num1;
+    scanf("%d", &num1);
+    printf("%d s = %d d %d h %d m %d s",num1,num1/86400,num1%86400/3600,num1%86400%3600/60,num1%86400%3600%60);
 
 
     return 0;

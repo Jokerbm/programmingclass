5ee6f8a9ba14f442dab3f20c735a208af806e052
RYU <ryuchotichan@gmail.com>
2020-02-01 12:47:24 -0800

    Perimeter of rectangle

---
+++ Perimeter of rectangle.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+    double num1,num2;
+    scanf("%lf\n%lf",&num1,&num2);
+    printf("Perimeter of rectangle = %.4lf units",num1 * 2 + num2 * 2);
+    return 0;
+}

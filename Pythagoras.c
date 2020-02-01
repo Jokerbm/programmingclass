0a74d81d4fbd808bd36651569a4bb666f8fed629
RYU <ryuchotichan@gmail.com>
2020-02-01 12:14:11 -0800

    Pythagoras

---
+++ Pythagoras.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    float num1,num2;
+    scanf("%f", &num1);
+    scanf("%f", &num2);
+    printf("%.2f",sqrt(pow(num1, 2) + pow(num2 , 2)));
+
+
+    return 0;
+}

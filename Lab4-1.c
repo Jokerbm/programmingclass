1d07b0cdee58cf740ea211e65f60f959bce1213a
ryu <ryuchotichan@gmail.com>
2020-02-08 13:18:22 -0500

    Lab4-1

---
+++ Lab4-1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+	int num1 , num2;
+	scanf("%d\n%d", &num1 , &num2);
+	if (num1 == num2){
+		printf("%d", num1 + num2);
+	}
+	else{
+		printf("%d", num1 - num2 );
+	}
+	return 0;
+}
